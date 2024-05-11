0a5124bbf88385a36cd0821b7c069a346704e246
yjc12138 <30204147@qq.com>
2024-05-11 22:54:56 +0800

    control

--- project_cpu/project_cpu.srcs/sources_1/new/top.v
+++ project_cpu/project_cpu.srcs/sources_1/new/top.v
@@ -7,13 +7,28 @@ module top(
     wire [31:0] imm;
     wire ALUSrc;
     wire [1:0] ALUOp;
-    wire [3:0] instruction;//指的是指令的30 和14-12 传入alucontrolport 命名会产生冲突！！！！！
+    wire [31:0] instruction;//指的是指令的30 和14-12 传入alucontrolport 命名会产生冲突！！！！！
     wire [3:0] ALUControl;
     wire [31:0] ALUResult;
     wire zero;
+    wire [31:0] inst;
+    wire Branch;
+    wire MemRead;
+    wire MemtoReg;
+    wire MemWrite;
+    wire RegWrite;
     
     CpuClk clk1(clkIn,cpuClk);
     ALUControlPort acp (ALUOp,instruction,ALUControl);
     ALU alu (readData1,readData2,imm,ALUSrc,ALUControl,ALUResult);
-    
+    control ctrl_instance (  
+            .instruction(instruction[6:0]),  
+            .Branch(Branch),  
+            .MemRead(MemRead),  
+            .MemtoReg(MemtoReg),  
+            .ALUOp(ALUOp),
+            .MemWrite(MemWrite),  
+            .ALUSrc(ALUSrc),  
+            .RegWrite(RegWrite)  
+        );  
 endmodule
