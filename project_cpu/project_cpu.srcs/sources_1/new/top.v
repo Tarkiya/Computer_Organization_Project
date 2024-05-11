3f52774cbf8ddc339f0b9aef76237933ff210c4e
wgjllzg <12212412@mail.sustech.edu.cn>
2024-05-11 21:44:24 +0800

    add top and clock

--- project_cpu/project_cpu.srcs/sources_1/new/top.v
+++ project_cpu/project_cpu.srcs/sources_1/new/top.v
@@ -1,2 +1,19 @@
-module top();
+module top(
+    input clkIn
+);
+    wire cpuClk;
+    wire [31:0]readData1;
+    wire [31:0] readData2;
+    wire [31:0] imm;
+    wire ALUSrc;
+    wire [1:0] ALUOp;
+    wire [3:0] instruction;//指的是指令的30 和14-12 传入alucontrolport 命名会产生冲突！！！！！
+    wire [3:0] ALUControl;
+    wire [31:0] ALUResult;
+    wire zero;
+    
+    CpuClk clk1(clkIn,cpuClk);
+    ALUControlPort acp (ALUOp,instruction,ALUControl);
+    ALU alu (readData1,readData2,imm,ALUSrc,ALUControl,ALUResult);
+    
 endmodule
