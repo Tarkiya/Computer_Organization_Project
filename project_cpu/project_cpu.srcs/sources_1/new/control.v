79a0a91269ac6db41625027e71f868593138ad3e
Tarkiya <1334753765@qq.com>
2024-05-11 21:15:13 +0800

    add ALU,ALUControl,control

---
+++ project_cpu/project_cpu.srcs/sources_1/new/control.v
@@ -0,0 +1,87 @@
+module control (
+    input [6:0] Instruction6_0,
+    output reg Branch,
+    output reg MemRead,
+    output reg MemtoReg,
+    output reg ALUOp,
+    output reg MemWrite,
+    output reg ALUSrc,
+    output reg RegWrite
+);
+
+always @* begin
+    case(Instruction6_0)
+        7'b1100011: 
+            begin
+            Branch = 1'b1; 
+            MemRead = 1'b0; 
+            MemtoReg = 1'b0; 
+            ALUOp = 2'b01; 
+            MemWrite = 1'b0; 
+            ALUSrc = 1'b0; 
+            RegWrite = 1'b0; //B  bne
+            end
+        7'b0110011: 
+            begin
+            Branch = 1'b0; 
+            MemRead = 1'b0; 
+            MemtoReg = 1'b0; 
+            ALUOp = 2'b10; 
+            MemWrite = 1'b0; 
+            ALUSrc = 1'b0; 
+            RegWrite = 1'b1; //R  add
+            end
+        7'b0010011: 
+            begin
+            Branch = 1'b0; 
+            MemRead = 1'b1; 
+            MemtoReg = 1'b1; 
+            ALUOp = 2'b00; 
+            MemWrite = 1'b0; 
+            ALUSrc = 1'b1; 
+            RegWrite = 1'b1; //I   lw  addi
+            end
+        7'b0100011: 
+            begin
+            Branch = 1'b0; 
+            MemRead = 1'b0; 
+            MemtoReg = 1'b1; 
+            ALUOp = 2'b00; 
+            MemWrite = 1'b1; 
+            ALUSrc = 1'b1; 
+            RegWrite = 1'b0; //S   sw
+            end
+        7'b0110111: 
+            begin
+            Branch = 1'b0; 
+            MemRead = 1'b0; 
+            MemtoReg = 1'b0; 
+            ALUOp = 2'b00; 
+            MemWrite = 1'b0; 
+            ALUSrc = 1'b0; 
+            RegWrite = 1'b1; //U   lui
+            end
+        7'b1101111: 
+            begin
+            Branch = 1'b1; 
+            MemRead = 1'b0; 
+            MemtoReg = 1'b0; 
+            ALUOp = 2'b00; 
+            MemWrite = 1'b0; 
+            ALUSrc = 1'b0; 
+            RegWrite = 1'b1; //J   jal
+            end
+        default:
+            begin
+            Branch = 1'b1; 
+            MemRead = 1'b0; 
+            MemtoReg = 1'b0; 
+            ALUOp = 2'b00; 
+            MemWrite = 1'b0; 
+            ALUSrc = 1'b0; 
+            RegWrite = 1'b1; //J   jal
+            end
+    endcase
+end
+
+endmodule
