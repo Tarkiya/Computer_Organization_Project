13e5be522dbb295af5e484410670324e7654b3b6
Tarkiya <1334753765@qq.com>
2024-06-01 23:57:53 +0800

    Merge remote-tracking branch 'origin/master'

---
+++ project_cpu/project_cpu.ip_user_files/sim_scripts/clock_cpu/ies/run.f
@@ -0,0 +1,15 @@
+-makelib ies_lib/xil_defaultlib -sv \
+  "E:/Digital_logic/Xilinx/Vivado/2017.4/data/ip/xpm/xpm_cdc/hdl/xpm_cdc.sv" \
+  "E:/Digital_logic/Xilinx/Vivado/2017.4/data/ip/xpm/xpm_memory/hdl/xpm_memory.sv" \
+-endlib
+-makelib ies_lib/xpm \
+  "E:/Digital_logic/Xilinx/Vivado/2017.4/data/ip/xpm/xpm_VCOMP.vhd" \
+-endlib
+-makelib ies_lib/xil_defaultlib \
+  "../../../../project_cpu.srcs/sources_1/ip/clock_cpu/clock_cpu_clk_wiz.v" \
+  "../../../../project_cpu.srcs/sources_1/ip/clock_cpu/clock_cpu.v" \
+-endlib
+-makelib ies_lib/xil_defaultlib \
+  glbl.v
+-endlib
+
