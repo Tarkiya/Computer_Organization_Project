f50120751d52e3c4d8bc6454701afee489d9ff92
Tarkiya <1334753765@qq.com>
2024-06-02 03:51:50 +0800

    Merge remote-tracking branch 'origin/master'

---
+++ project_cpu/project_cpu.srcs/sim_1/new/scene001.v
@@ -0,0 +1,34 @@
+
+module scene001();
+  reg clkIn;
+  reg rst;
+  reg [4:0] button;
+  reg [15:0] switch;
+  wire [15:0] led;
+  top uut (
+    .clkIn(clkIn),
+    .rst(rst),
+    .button(button),
+    .switchLeft(switch[15:8]),
+    .switchRight(switch[7:0]),
+    .ledLeft(led[15:8]),
+    .ledRight(led[7:0])
+  );
+  initial begin
+      clkIn = 0;
+      forever #5000 clkIn = ~clkIn; // 100 MHz clock
+    end
+  initial begin
+    rst = 0;
+    button = 5'b00000;
+    switch = 16'hff0f;
+    #3000000 rst = 1;
+    #50000 button = 5'b00001;
+    #50000 button = 5'b00000;
+    #100000 switch = 16'habcd;
+    #100000 button = 5'b00001;
+    #50000 button = 5'b00000;
+    #1000000000 $finish;
+  end
+
+endmodule
